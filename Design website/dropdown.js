5758845d48b3de669dbd23e5f73bbc4149747071
roblogtenberg <rob_logten@hotmail.com>
2015-09-23 12:02:38 +0200

    Dingen

--- Design website/dropdown.js
+++ Design website/dropdown.js
@@ -1,43 +1,25 @@
-var timeout	= 500;
-var closetimer	= 0;
-var ddmenuitem	= 0;
-
+var opened = false;
 // close layer when click-out
 // this causes the menu to never open!! fix this shit!
-document.onclick = mclose;
+// document.onclick = mclose;
 
 // open hidden layer
 function mopen(id)
 {	
-	// cancel close timer
-	mcancelclosetime();
-
-	// close old layer
-	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
-
 	// get new layer and show it
 	ddmenuitem = document.getElementById(id);
-	ddmenuitem.style.visibility = 'visible';
+
+	if(opened) {
+		ddmenuitem.style.visibility = 'hidden';
+		opened = false;
+	} else {
+		ddmenuitem.style.visibility = 'visible';
+		opened = true;
+	}
 
 }
 // close showed layer
 function mclose()
 {
 	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
-}
-
-// go close timer
-function mclosetime()
-{
-	closetimer = window.setTimeout(mclose, timeout);
-}
-
-// cancel close timer
-function mcancelclosetime()
-{
-	if(closetimer)
-	{
-		window.clearTimeout(closetimer);
-		closetimer = null;
-	}
 }
