74a25cd701000ad56e08a3481eb06247b095d974
victorerik13 <v_vd_riet@hotmail.com>
2015-10-09 13:48:15 +0200

    add user, get userlist

--- RESTServer/src/model/Model.java
+++ RESTServer/src/model/Model.java
@@ -6,11 +6,21 @@ import java.util.List;
 public class Model {
 
 	private List<Movie> movies = new ArrayList<>();
-	
+	private List<User> users = new ArrayList<>();
+
 	public Model() {
 		movies.add(new Movie(1, "2", "Maze runner", "5-10-2015", 120, "Michael Bay", "description"));
 	}
-	
+
+	public void addUser(User user) {
+		users.add(user);
+		System.out.println("User added");
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
 	public List<Movie> getMovies() {
 		return movies;
 	}
--- RESTServer/src/resources/UserResource.java
+++ RESTServer/src/resources/UserResource.java
@@ -1,5 +1,7 @@
 package resources;
 
+import java.util.ArrayList;
+
 import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -11,24 +13,26 @@ import javax.ws.rs.core.MediaType;
 
 import model.Model;
 import model.Movie;
+import model.User;
 
 @Path("/users")
 public class UserResource {
 
-	private String firstname;
+	private String surname;
 	private String prefix;
 	private String lastname;
 	private String nickname;
 
 	@Context
 	ServletContext context;
+	Model model = new Model();
 
 	@POST
 	// @Produces({ MediaType.APPLICATION_XML })
-	public Object getParameter(@QueryParam("firstname") String firstname, @QueryParam("prefix") String prefix,
+	public Object getParameter(@QueryParam("surname") String surname, @QueryParam("prefix") String prefix,
 			@QueryParam("lastname") String lastname, @QueryParam("nickname") String nickname) {
-		if (firstname != null) {
-			this.firstname = firstname;
+		if (surname != null) {
+			this.surname = surname;
 		}
 
 		if (prefix != null) {
@@ -42,11 +46,20 @@ public class UserResource {
 		if (nickname != null) {
 			this.nickname = nickname;
 		}
+		User user = new User(surname, prefix, lastname, nickname);
+		model.addUser(user);
 		return showAccount();
 	}
 
+	@Path("/get")
+	@GET
+	// @Produces({ MediaType.APPLICATION_XML })
+	public ArrayList<User> getParameter(@QueryParam("getUsers") String getUsers) {
+		return (ArrayList<User>) model.getUsers();
+	}
+
 	public String showAccount() {
-		String account = this.firstname + " " + this.prefix + " " + this.lastname + " " + this.nickname;
+		String account = this.surname + " " + this.prefix + " " + this.lastname + " " + this.nickname;
 		return account;
 	}
 
