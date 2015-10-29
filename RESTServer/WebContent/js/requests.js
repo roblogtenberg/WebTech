384991aac8b8c62723e6137aeee90c14d2e27745
victorerik13 <v_vd_riet@hotmail.com>
2015-10-29 11:56:24 +0100

    Added page CreateUser

--- RESTServer/WebContent/js/requests.js
+++ RESTServer/WebContent/js/requests.js
@@ -14,19 +14,28 @@ function getPoster(movieName) {
 }
 
 function loginCheck(loginData, nickname) {
-    $.ajax({
-          type:     "post",
-          contentType: "application/x-www-form-urlencoded",
-          url:      "api/authentication",
-          data: loginData,
-          dataType: "text",
-          error: function(xhr, status, error) {
-            alert("Uw inlognaam of wachtwoord is verkeerd");
-          },
-          success: function (data) {
-            $("#loginForm").css("display", "none");
-            $("#submitButton").css("display", "none");
-            $("#result").append("Welkom," + nickname);
-          }
-        });
+  $.ajax({
+    type:     "post",
+    contentType: "application/x-www-form-urlencoded",
+    url:      "api/authentication",
+    data: loginData,
+    dataType: "text",
+    error: function(xhr, status, error) {
+      alert("Uw inlognaam of wachtwoord is verkeerd");
+    },
+    success: function (data) {
+      $("#loginForm").css("display", "none");
+      $("#submitButton").css("display", "none");
+      $("#result").append("Welkom," + nickname);
+    }
+  });
+}
+
+function createUser(registerData, wachtwoord) {
+  $.ajax({
+    type:   "post",
+    contentType: "application/x-www-form-urlencoded",
+    url: "api/users"
+
+  })
 }
